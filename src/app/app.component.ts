4a9c88afff9dbbe04e134bd15a71dd7f0a12200f
nir@500tech.com <nir@500tech.com>
2016-10-18 20:58:50 +0100

    adding bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from "@angular/core";
 import {Store} from "./app.store";
 
 
 @Component({
-  selector   : 'app',
-  template: `<h1>Redux Angular Workshop</h1>`,
+  selector     : 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls    : ['../assets/bootstrap.css'],
+  template     : `<h1>Redux Angular Workshop</h1>`,
 })
 
 export class AppComponent {
 
-  constructor(store: Store) {
+  constructor(store:Store) {
     console.log(store.state);
   }
 }
